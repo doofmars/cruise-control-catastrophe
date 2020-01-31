e99795b24e365022cd53aa4064ba0d925a3b8345
doofmars <jan-preuss@gmx.de>
2020-01-31 22:25:44 +0100

    Add basic fire logic and rooms setup

---
+++ Assets/Scripts/Firestarter.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Firestarter : MonoBehaviour
+{
+    public int fireRate = 20;
+    public float waitTime = 5.0f;
+
+    private float timer = 0.0f;
+    private float visualTime = 0.0f;
+    private long timeSinceLastFire = 0;
+    private object[] rooms;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rooms = FindObjectsOfType(typeof(Room));
+        Debug.Log(rooms.Length);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer += Time.deltaTime;
+
+        // Check if we have reached beyond 2 seconds.
+        // Subtracting two is more accurate over time than resetting to zero.
+        if (timer > waitTime)
+        {
+            visualTime = timer;
+
+            // Remove the recorded 2 seconds.
+            timer = timer - waitTime;
+
+            if (Random.Range(0, 100) < fireRate)
+            {
+                //Burn a room
+                int roomNumber = Random.Range(0, rooms.Length);
+                Debug.Log("fire in room " + roomNumber);
+
+            }
+
+            
+        }
+    }
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player
+{
+    public int health;
+    public bool isDead;
+    public Color color;
+    public GameObject gameObject;
+
+    public Player(int health, Color color)
+    {
+        this.health = health;
+        this.isDead = false;
+        this.color = color;
+        this.gameObject = null;
+    }
+}
