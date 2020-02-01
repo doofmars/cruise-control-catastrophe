a0d24d5fb550dbc071d366459ef1baedc2f3d776
Felix <site-github@penth.in>
2020-02-01 13:57:06 +0100

    basic main-menu added various meta files

---
+++ Assets/Scripts/MainMenuScript.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuScript : MonoBehaviour
+{
+    public string sceneName;
+
+
+    public void exitGame()
+    {
+        Debug.Log("Exit");
+        Application.Quit();
+    }
+
+    public void showSettings()
+    {
+
+    }
+
+    public void showCredits()
+    {
+
+    }
+
+    public void startGame()
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+
+}
