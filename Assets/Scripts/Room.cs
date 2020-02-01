6b87efef8f9563a5a3c67143e300365d4fd3cd78
aeolus3000 <dev@skas.de>
2020-02-01 09:51:04 +0100

    Add fire animation

--- Assets/Scripts/Room.cs
+++ Assets/Scripts/Room.cs
@@ -5,12 +5,30 @@ using UnityEngine;
 public class Room : MonoBehaviour
 {
     public bool isOnFire = false;
+    // Reference to the Prefab. Drag a Prefab into this field in the Inspector.
+    public GameObject myPrefab;
+
+    private bool isFireDisplayed = false;
 
     private void OnTriggerEnter2D(Collider2D other)
     {
+        Instantiate(myPrefab, new Vector3(0, 0, 0), Quaternion.identity);
         if (other.tag == "Player")
         {
             Debug.Log("player is in Room");
         }
     }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isOnFire && !isFireDisplayed)
+        {
+            isFireDisplayed = true;
+
+            // Instantiate at position (0, 0, 0) and zero rotation.
+            Instantiate(myPrefab, GetComponent<Collider2D>().offset, Quaternion.identity);
+        }
+    }
+
 }
