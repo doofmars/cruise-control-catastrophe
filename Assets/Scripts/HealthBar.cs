e630e9ae28ac0a27e1e8673faf66d92741f493c2
AplusKminus <apluskminus@gmail.com>
2020-02-01 13:05:03 +0100

    Add new Movement System, Prefabs for HealthBar

---
+++ Assets/Scripts/HealthBar.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthBar : MonoBehaviour
+{
+    public Transform bar;
+    private Vector3 vector3 = new Vector3(1f, 1f);
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+    }
+
+    public void SetValue(float value)
+    {
+        vector3.x = value;
+        bar.localScale = vector3;
+    }
+}
